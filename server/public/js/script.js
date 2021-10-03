4c529fe67e8ecf0b7d8609a02ef788a391621b93
st0ro <46041969+st0ro@users.noreply.github.com>
2021-10-03 13:40:46 -0600

    filtering

--- server/public/js/script.js
+++ server/public/js/script.js
@@ -1,15 +1,19 @@
-$.ajax({
-    url: "/getusers",
-    type: "GET",
-    success: function (res){
-        var json = JSON.parse(res)
-        var innerhtml = `<table class="table table-hover">
-          <thead class="thead-dark">
+var logs;
+var blacklistUsers = [], whitelistTags = []
+
+function loadUsers() {
+    $.ajax({
+        url: "/getusers",
+        type: "GET",
+        success: function (res) {
+            var json = JSON.parse(res)
+            var innerhtml = `<table class="table table-hover">
+          <thead">
           <colgroup>
-           <col span="1" style="width: 20%;">
-           <col span="1" style="width: 30%;">
-           <col span="1" style="width: 20%;">
-           <col span="1" style="width: 30%;">
+           <col span="1" style="width: 10%;">
+           <col span="1" style="width: 35%;">
+           <col span="1" style="width: 15%;">
+           <col span="1" style="width: 40%;">
           </colgroup>
           <tr>
             <th scope="col">Select</th>
@@ -19,58 +23,118 @@ $.ajax({
           </tr>
           </thead>
           <tbody>`;
-        json.forEach(function (element){
-            innerhtml += `<tr>
-            <td><input type="checkbox" class="selectUserInput" linked-user="${element.name}"></input></td>
+            json.forEach(function (element) {
+                innerhtml += `<tr>
+            <td><input type="checkbox" class="selectUserInput" data-user="${element.name}" checked></input></td>
             <td>${element.name}</td>
             <td>${element.seat}</td>
             <td>${element.position}</td>
           </tr>`
-        })
-        innerhtml +=`</tbody>
+            })
+            innerhtml += `</tbody>
         </table>`
 
-        document.getElementById("nav-user").innerHTML = innerhtml
-    }
-})
+            document.getElementById("nav-user").innerHTML = innerhtml
+            let users = document.getElementsByClassName("selectUserInput")
+            Array.from(users).forEach(element => {
+                element.addEventListener('input', updateUserFilter)
+            })
+        }
+    })
+}
 
-$.ajax({
-    url: "/gettags",
-    type: "GET",
-    success: function (res){
-        var json = JSON.parse(res)
-        var innerhtml = `<table class="table table-hover">
-          <thead class="thead-dark">
+function loadTags() {
+    $.ajax({
+        url: "/gettags",
+        type: "GET",
+        success: function (res) {
+            var json = JSON.parse(res)
+            whitelistTags = JSON.parse(res)
+            var innerhtml = `<table class="table table-hover">
           <colgroup>
            <col span="1" style="width: 20%;">
            <col span="1" style="width: 80%;">
           </colgroup>
+          <thead>
           <tr>
             <th scope="col">Select</th>
             <th scope="col">Category</th>
           </tr>
           </thead>
           <tbody>`;
-        json.forEach(function (element){
-            innerhtml += `<tr>
-            <td><input type="checkbox" class="selectTagInput" linked-tag="${element}"></input></td>
+            json.forEach(function (element) {
+                innerhtml += `<tr>
+            <td><input type="checkbox" class="selectTagInput" data-tag="${element}" checked></input></td>
             <td>${element.charAt(0).toUpperCase() + element.slice(1)}</td>
           </tr>`
-        })
-        innerhtml +=`</tbody>
+            })
+            innerhtml += `</tbody>
         </table>`
 
-        document.getElementById("nav-group").innerHTML = innerhtml
-    }
-})
+            document.getElementById("nav-group").innerHTML = innerhtml
+            let tags = document.getElementsByClassName("selectTagInput")
+            Array.from(tags).forEach(element => {
+                element.addEventListener('input', updateTagFilter)
+            })
+        }
+    })
+}
 
-$.ajax({
-    url: "/getlogs",
-    type: "GET",
-    success: function (res){
-        var json = JSON.parse(res)
-        var innerhtml = `<table class="table table-hover">
-          <thead class="thead-dark">
+function loadLogs() {
+    $.ajax({
+        url: "/getlogs",
+        type: "GET",
+        success: function (res) {
+            logs = JSON.parse(res)
+            fillConsoleLogs(logs)
+        }
+    })
+}
+
+function updateUserFilter(event) {
+    if(event.target.checked){
+        const index = blacklistUsers.indexOf(event.target.dataset.user);
+        if (index > -1) {
+            blacklistUsers.splice(index, 1);
+        }
+    }
+    else{
+        blacklistUsers.push(event.target.dataset.user)
+    }
+    fillConsoleLogs()
+}
+
+function updateTagFilter(event) {
+    if(!event.target.checked){
+        const index = whitelistTags.indexOf(event.target.dataset.tag);
+        if (index > -1) {
+            whitelistTags.splice(index, 1);
+        }
+    }
+    else{
+        whitelistTags.push(event.target.dataset.tag)
+    }
+    fillConsoleLogs()
+}
+
+function fillConsoleLogs(logs) {
+    let newLogs = JSON.parse(JSON.stringify(logs))
+    let i = 0;
+    while(i < newLogs.length){
+        if(blacklistUsers.includes(newLogs[i].user) || !newLogs[i].tags.some(r=>whitelistTags.includes(r))){
+            newLogs.splice(i, 1);
+        } else {
+            ++i;
+        }
+    }
+    let innerhtml = `<table class="table table-hover">
+          <colgroup>
+            <col span="1" style="width: 15%;">
+            <col span="1" style="width: 10%;">
+            <col span="1" style="width: 20%;">
+            <col span="1" style="width: 55%;">
+          </colgroup>
+          <thead>
           <tr>
             <th scope="col">Timestamp</th>
             <th scope="col">User</th>
@@ -79,17 +143,27 @@ $.ajax({
           </tr>
           </thead>
           <tbody>`;
-        json.forEach(function (element){
-            innerhtml += `<tr>
+    newLogs.forEach(function (element) {
+        innerhtml += `<tr>
             <td>${element.timestamp}</td>
             <td>${element.user}</td>
-            <td>${element.tags}</td>
+            <td>`
+        element.tags.forEach(function (tag) {
+            innerhtml += `<span class="p-2 bg-secondary sp-tag">${tag.charAt(0).toUpperCase() + tag.slice(1)}</span>`
+        })
+        innerhtml += `</td>
             <td>${element.content}</td>
           </tr>`
-        })
-        innerhtml +=`</tbody>
+    })
+    innerhtml += `</tbody>
         </table>`
 
-        document.getElementById("consoleTable").innerHTML = innerhtml
-    }
-})
+    document.getElementById("consoleTable").innerHTML = innerhtml
+}
+
+setTimeout(function () {
+    loadUsers()
+    loadTags()
+    loadLogs()
+    setInterval(loadUsers, 1000)
+}, 200)
